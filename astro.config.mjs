df7c56adbb242174e7b2a1d52498ed8ff4191f95
ao-x <admin@ao-x.cn>
2025-02-28 22:45:04 +0800

    设置基础

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,21 +5,39 @@ export default defineConfig({
 	site: 'https://mirror.winx.run',
 	integrations: [
 		starlight({
-			title: 'WinX-M',
-			logo: {src: './src/assets/winx.webp',},
-			favicon: '/favicon.ico',
-			pagination: false,
-			social: {
-				github: 'https://github.com/winx-ipfs/Mirror-Web',
+			title: 'WinX-M', // 设置网站标题
+			description: '一个基于 IPFS 的镜像站', // 设置网站描述
+			logo: { // 在导航栏中设置一个 logo 图片
+				light: './src/assets/winx-light.webp', // 导航栏中浅色模式 logo 图片
+				dark: './src/assets/winx-dark.webp',}, // 导航栏中深色模式 logo 图片
+			tableOfContents: { // 配置每个页面右侧显示的目录
+				minHeadingLevel: 2, // 从 <h2> 开始
+				maxHeadingLevel: 4, }, // 到 <h4> 结束
+			editLink: { // 启用 “编辑此页” 链接
+				baseUrl: 'https://github.com/winx-ipfs/Mirror-Web/edit/main/', // 设置 “编辑此页” 链接
 			},
-			locales: {
-				root: {
-					label: '简体中文',
-					lang: 'zh-CN',
+			locales: { // 设置多语言
+				root: { // 不用设置语言目录
+					label: '简体中文', // 设置网站支持简体中文作为语言
+					lang: 'zh-CN', // 向浏览器告知页面语言
+					dir: 'ltr', // 设置网站为从左到右
 				},
 			},
-			head: [
-				// 示例：添加 Fathom 分析脚本标签。
+			defaultLocale: 'zh-CN', // 为网站设置简体中文作为默认语言
+			social: { // 社交媒体账户详情
+				github: 'https://github.com/winx-ipfs/Mirror-Web', // 添加 GitHub 社交媒体账户详情
+			},
+			// customCss: [ // 使用 CSS 文件来自定义 Starlight 网站的外观和风格
+			// 		'./src/custom-styles.css', // 使用本地css
+			// 		'@fontsource/roboto' // 使用模块css
+			// ],
+			expressiveCode: { // 代码块设置
+				styleOverrides: { // 代码块样式
+					borderRadius: '0.5rem' }, // 设置 0.5rem 的圆角
+			},
+			pagefind: true, // 开启网站搜索
+			prerender: true, // 让网站为静态生成
+			head: [ // 自定义标签到 Starlight 网站的 <head> 中
 				{
 					tag: 'link',
 					attrs: {
@@ -28,7 +46,19 @@ export default defineConfig({
 					},
 				},
 			],
-			sidebar: [
+			lastUpdated: false, // 控制页脚是否显示页面上次更新的时间
+			pagination: false, // 定义页脚是否应包含上一页和下一页的链接
+			favicon: '/favicon.ico', // 设置网站的默认 favicon 的路径
+			titleDelimiter: '|', // 设置在页面的 <title> 标签里页面标题和网站标题之间的分隔符
+			disable404Route: false, // 自定义 404 页面
+			components: { // 提供组件的路径来覆盖 Starlight 的默认实现
+				// SocialLinks: './src/components/MySocialLinks.astro',
+			},
+			plugins: [ // 使用自定义插件扩展 Starlight
+				// starlightPlugin()
+			],
+			credits: true, // 启用网站页脚显示 “基于 Starlight 构建” 的链接。
+			sidebar: [ // 配置网站的侧边栏导航项目
 				{
 					label: '首页',
 					link: '/',
